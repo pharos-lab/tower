53ea82e3fcdeb130faa22b22eff7c90aa2a68345
mthdht <mthdht@gmail.com>
2023-03-28 10:54:53 +0200

    start page builder layout

---
+++ src/components/TPageBuilder.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>Page Builder</h2>
+  <section class="t-page-builder h-full bg-slate-200 flex">
+    <aside class="t-toolbar w-56 bg-blue-200"></aside>
+    <div class="canvas bg-red-200 grow"></div>
+  </section>
+</template>
+
+<script setup>
+const props = defineProps({});
+</script>
+
+<style scoped></style>
---
+++ src/components/template.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class=""></div>
+</template>
+
+<script setup>
+const props = defineProps({});
+</script>
+
+<style scoped></style>
