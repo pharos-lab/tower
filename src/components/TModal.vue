a71d4d1e82304cc6847d73bf8c5926c752255fa1
mthdht <mthdht@gmail.com>
2023-03-30 08:35:53 +0200

    add modal to button for creating section

--- src/components/TModal.vue
+++ src/components/TModal.vue
@@ -1,10 +1,4 @@
 <template>
-  <div class="inline-block l-modal-trigger" @click="isOpen = true">
-    <slot name="label">
-      {{ props.label }}
-    </slot>
-  </div>
-
   <teleport to="body">
     <div
       class="
@@ -17,14 +11,22 @@
         pt-64
         bg-slate-500/50
       "
-      v-show="isOpen"
-      @click.self="isOpen = false"
+      v-show="props.show"
+      @click.self="$emit('closeModal')"
     >
       <div class="l-modal-content w-2/3 opacity-100 bg-white relative">
-        <XCircleIcon
+        <XMarkIcon
           v-if="props.dismissable"
-          @click="isOpen = false"
-          class="right-2 absolute top-2 text-slate-600"
+          @click="$emit('closeModal')"
+          class="
+            right-2
+            absolute
+            top-2
+            text-slate-600
+            h-6
+            w-6
+            hover:text-slate-800
+          "
         />
 
         <div class="l-modal-header p-4">
@@ -44,20 +46,17 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import { ref, computed } from 'vue';
-import XCircleIcon from '@heroicons/vue/24/outline';
+import { ref } from 'vue';
+import { XMarkIcon } from '@heroicons/vue/24/outline';
 
+const emits = defineEmits(['closeModal']);
 const props = defineProps({
-  label: {
-    type: String,
-  },
   dismissable: {
     type: Boolean,
     default: true,
   },
+  show: Boolean,
 });
-
-const isOpen = ref(false);
 </script>
 
 <style scoped></style>
--- src/components/TPageBuilder.vue
+++ src/components/TPageBuilder.vue
@@ -1,37 +1,35 @@
 <template>
-  <h2>Page Builder</h2>
   <section class="t-page-builder h-full flex">
     <TSidebar />
     <div class="canvas bg-red-200 grow">
       <div class="t-add-section flex justify-center items-center h-40">
-        <TModal>
-          <template #label>
-            <button
-              class="
-                t-add-section-action
-                w-48
-                h-20
-                pointer-cursor
-                border-2 border-dashed border-sky-500
-                bg-sky-50
-              "
-            >
-              Add section
-            </button>
-          </template>
-        </TModal>
+        <button
+          class="
+            t-add-section-action
+            w-48
+            h-20
+            pointer-cursor
+            border-2 border-dashed border-sky-500
+            bg-sky-50
+          "
+          @click="showModal = true"
+        >
+          Add section
+        </button>
+        <TModal :show="showModal" @closeModal="showModal = false"> </TModal>
       </div>
     </div>
   </section>
 </template>
 
 <script setup>
-
-
+import { ref } from 'vue';
 import TSidebar from './TSidebar.vue';
 import TModal from './TModal.vue';
 
 const props = defineProps({});
+
+const showModal = ref();
 </script>
 
 <style scoped>
