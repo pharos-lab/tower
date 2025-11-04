1b7426b92db31838e391d119c8fbee1c4f64e338
mth <mthdht@gmail.com>
2025-11-04 09:48:58 +0000

    add shadcn vue

--- src/components/AddPanel.vue
+++ src/components/AddPanel.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-54 bg-slate-200 max-h-full overflow-y-auto shrink-0">
+    <div class="w-54 bg-slate-200 max-h-full overflow-y-auto shrink-0 p-2">
         
     </div>
 </template>
