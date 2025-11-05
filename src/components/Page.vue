83ebdc0cf56d3ec9b02450df384c7be0ef7fb369
mth <mthdht@gmail.com>
2025-11-05 07:16:51 +0000

    add-panel: add current section state

--- src/components/Page.vue
+++ src/components/Page.vue
@@ -5,7 +5,12 @@ import { pageBuilder } from '@/stores/store';
 
 <template>
     <div class="grow bg-slate-100 max-h-full overflow-y-auto p-1">
-        <section v-for="section in pageBuilder.sections" class="relative group hover:ring-2 ring-red-500">
+        <section 
+            v-for="section in pageBuilder.sections" 
+            class="relative group hover:ring-2 hover:ring-red-500" 
+            :class="{'ring-2 ring-emerald-500': pageBuilder.currentSection == section }"
+            @click="pageBuilder.currentSection = section"
+        >
             <button class="hidden absolute -top-3 left-1/2 -translate-x-1/2 group-hover:block" @click="pageBuilder.removeSection(section.id)">supprimer</button>
             <pre>{{ section }}</pre>
         </section>
--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,26 +1,34 @@
 import { reactive } from 'vue'
 import type { PageBuilder } from '../types'
-import type { Layout, Section } from '../types'
+import type { Layout, Section, Block } from '../types'
 
 const generateId = () => `${Date.now()}-${Math.random().toString(36).substring(2, 9)}`
 
 export const pageBuilder: PageBuilder = reactive({
     title: '',
     sections: [] as Section[],
-    currentSection: null,
-    currentBlock: null,
+    currentSection: null as Section | null,
+    currentBlock: null as Block | null,
     
     addSection(layout: Layout) {
-        this.sections.push({
+        const section: Section = {
             id: generateId(),
             layout: layout,
-        })
+        }
 
-        console.log(this);
+        this.sections.push(section)
+
+        this.currentSection = section
     },
 
     removeSection(sectionId: string) {
         const index = this.sections.findIndex(section => section.id === sectionId)
+
+        
+        if (this.currentSection?.id === sectionId) {
+            this.currentSection = null
+        }
+
         if (index > -1) {
             this.sections.splice(index, 1)
         }
