a316cd15eddde07d87b71a3aee6beb148f27308d
mth <mthdht@gmail.com>
2025-11-05 08:37:44 +0000

    add-panel: add tabs for sections, blocks and components

--- src/components/AddPanel.vue
+++ src/components/AddPanel.vue
@@ -1,16 +1,35 @@
 <template>
-    <div class="w-54 max-h-full overflow-y-auto shrink-0 p-2">
-        <div class="flex gap-2">
-            <Card 
-                class="justify-center aspect-square items-center w-1/2 gap-2" 
-                v-for="layout in layouts" 
-                :key="layout.name"
-                @click="pageBuilder.addSection(layout)"
-            >
-                <component :is="layout.icon"></component>
-                <p class="text-sm font-semibold text-slate-900/70">{{ layout.name }}</p>
-            </Card>
-        </div>
+    <div class="w-64 max-h-full overflow-y-auto shrink-0 p-2">
+        <Tabs default-value="sections">
+            <TabsList>
+                <TabsTrigger value="sections">Sections</TabsTrigger>
+                <TabsTrigger value="blocks">Blocks</TabsTrigger>
+                <TabsTrigger value="components">Components</TabsTrigger>
+            </TabsList>
+
+            <TabsContent value="sections">
+                <div class="grid grid-cols-2 gap-2 flex-wrap">
+                    <Card 
+                        class="justify-center aspect-square items-center gap-2 hover:bg-slate-100" 
+                        v-for="layout in layouts" 
+                        :key="layout.name"
+                        @click="pageBuilder.addSection(layout)"
+                    >
+                        <component :is="layout.icon"></component>
+                        <p class="text-sm font-semibold text-slate-900/70">{{ layout.name }}</p>
+                    </Card>
+                </div>
+            </TabsContent>
+
+            <TabsContent value="blocks">
+                blocks
+            </TabsContent>
+
+            <TabsContent value="components">
+                components
+            </TabsContent>
+        </Tabs>
+        
     </div>
 </template>
 
@@ -18,5 +37,6 @@
 import { Card } from './ui/card';
 import { layouts } from '@/utils/layouts';
 import { pageBuilder } from '../stores/store.ts'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from './ui/tabs';
 
 </script>
--- src/utils/layouts.ts
+++ src/utils/layouts.ts
@@ -1,15 +1,25 @@
-import { Home } from "lucide-vue-next"
+import { Columns2, Columns3, Columns4, Square } from "lucide-vue-next"
 import type { Layout } from "../types"
 
 export const layouts: Layout[] = [
     {
         cols: 1,
-        icon: Home,
-        name: 'One Column'
+        icon: Square,
+        name: '1 Column'
     },
     {
         cols: 2,
-        icon: Home,
-        name: 'two Column'
-    }
+        icon: Columns2,
+        name: '2 Columns'
+    },
+    {
+        cols: 3,
+        icon: Columns3,
+        name: '3 Columns'
+    },
+    {
+        cols: 4,
+        icon: Columns4,
+        name: '4 Column'
+    },
 ]
